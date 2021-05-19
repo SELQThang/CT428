5f44fbc583cc4214310bb3080216cc1ce5091cc8
SELQThang <65080259+SELQThang@users.noreply.github.com>
2021-05-19 14:25:22 +0700

    Delete buoi4 directory

--- buoi4/connectDB.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "id16021442_buoi3");
-$conn->set_charset("utf8");
-if ($conn->connect_error) {
-    die("Connection fail: " . $conn->connect_error);
-}
